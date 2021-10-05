bf85b9956371bc621d1c2cc2baa469bac91464a0
KAR-NG <karhou.0426@gmail.com>
2021-10-05 13:41:36 +1000

    update

--- about.md
+++ about.md
@@ -1,2 +1,63 @@
-#### Regarding this 
+### Description of this dataset
+
+##### [ Target variable to Predict]
+
+**house.value**	- Median value of owner-occupied homes in per $1000s.
+
+##### [ Predictors ]
+
+**charles.river** -	Charles River dummy variable (= 1 if tract bounds river; 0 otherwise).
+
+**crime.rate** - Per capita crime rate by town.
+
+**resid.zone** - Proportion of residential land zoned for lots over 25,000 sq.ft.
+
+**indus.biz** -	Proportion of non-retail business acres per town.
+
+**nitrogen.oxide** - Nitrogen oxides concentration (parts per 10 million).
+
+**room** - Average number of rooms per dwelling.
+
+**age** - Proportion of owner-occupied units built prior to 1940.
+
+**dist.to.work** - Weighted mean of distances to five Boston employment centres.
+
+**highway.index** -	Index of accessibility to radial highways.
+
+**property.tax** - Full-value property-tax rate per per $10,000.
+
+**pt.ratio** - Pupil-teacher ratio by town.
+
+**black**	- 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town.
+
+**lstat**	- lower status of the population (percent).
+
+
+
+### About This Prediction
+
+This prediction uses a machine learning algorithm, **Random Forest**, to make your predictions. It is the best algorithm in the case of this dataset after comparing its performance metrics with various potential algorithm candidates included:
+
+* Lasso Regression  
+* PLS  
+* Decision Tree   
+* K-Nearest Neightbour (KNN)  
+* XGBoost  
+
+This Random Forest had the highest R-squared value at nearly 90% with the lowest prediction error rate at 13.43%. R-squared value is an internal performance metric that indicates how well prediction power this random forest model has.
+
+### Disclaimer
+
+This is not the ordinary Boston dataset. Several minor adjustments had been made to the predictors' data to make it special and unique. The aim of this project and this application are for demonstration purposes only. 
+
+### Creator
+
+Author: Kar Ng
+
+Date: October 2021
+
+Github: https://github.com/KAR-NG 
+
+
+
 
--- app.R
+++ app.R
@@ -6,6 +6,8 @@ library(shiny)
 library(data.table)
 library(randomForest)
 library(shinythemes)
+library(tidyverse)
+
 
 #--------------------------------------------------------------------------
 # Read in the model -------------------------------------------------------
@@ -15,8 +17,6 @@ model_rf <- readRDS("boston_rf_model.rds")
 
 train_data <- read.csv("train.csv", header = T)
 
-summary(train_data)
-
 #--------------------------------------------------------------------------
 # User Interface  ---------------------------------------------------------
 #--------------------------------------------------------------------------
@@ -25,70 +25,75 @@ summary(train_data)
 ui <- fluidPage(theme = shinytheme("cyborg"),
   
   # Page header
-  navbarPage("Boston Median House Prices Predictor"),
+  navbarPage("Boston Median House Prices Predictor",
   
   # Input values
   tabPanel("Home",
-  sidebarPanel(
-    tags$label(h3("Input Features")),
-    selectInput(inputId = "charles.river", 
-                label = "Charles River", 
-                choices = list("Yes" = 1, "No" = 0)),
-    sliderInput(inputId = "crime.rate",
-                label = "Crime Rate",
-                value = 0,
-                min = min(train_data$crime.rate),
-                max = max(train_data$crime.rate)),
-    sliderInput(inputId = "resid.zone", 
-                label = "Residential Zone",
-                value = 0,
-                min = min(train_data$resid.zone),
-                max = max(train_data$resid.zone)),
-    sliderInput(inputId = "indus.biz", 
-                label = "Non-retail business",
-                value = 0,
-                min = min(train_data$indus.biz),
-                max = max(train_data$indus.biz)),
-    sliderInput(inputId = "nitrogen.oxide", 
-                label = "Nitrogen Oxide Concentration",
-                value = 0,
-                min = min(train_data$nitrogen.oxide),
-                max = max(train_data$nitrogen.oxide)
-                 ),
-    numericInput(inputId = "room", 
-                 label = "Room",
-                 value = 0),
-    numericInput(inputId = "age", 
-                 label = "Age",
-                 value = 0),
-    numericInput(inputId = "dist.to.work", 
-                 label = "Distance to Employment Center",
-                 value = 0),
-    numericInput(inputId = "highway.index", 
-                 label = "Radial highways Accessibility",
-                 value = 0),
-    numericInput(inputId = "property.tax", 
-                 label = "Property Tax",
-                 value = 0),
-    numericInput(inputId = "pt.ratio", 
-                 label = "Pupil-teacher ratio",
-                 value = 0),
-    numericInput(inputId = "black", 
-                 label = "Black Community Proportion",
-                 value = 0),
-    numericInput(inputId = "lstat", 
-                 label = "Lower Status Proportion",
-                 value = 0),
-    actionButton("SubmitButton", "Submit", class = "btn btn-primary")),
+           sidebarPanel(
+             tags$label(h3("Input Features")),
+             selectInput(inputId = "charles.river", 
+                      label = "Charles River", 
+                      choices = list("Yes" = 1, "No" = 0)),
+             sliderInput(inputId = "crime.rate",
+                          label = "Crime Rate",
+                          value = 1,
+                          min = min(train_data$crime.rate),
+                          max = max(train_data$crime.rate)),
+             sliderInput(inputId = "resid.zone", 
+                         label = "Residential Zone",
+                         value = 1, 
+                         min = min(train_data$resid.zone),
+                         max = max(train_data$resid.zone)),
+             sliderInput(inputId = "indus.biz", 
+                         label = "Non-retail business",
+                         value = 1,
+                         min = min(train_data$indus.biz),
+                         max = max(train_data$indus.biz)),
+            sliderInput(inputId = "nitrogen.oxide", 
+                        label = "Nitrogen Oxide Concentration",
+                        value = 0.4,
+                        min = min(train_data$nitrogen.oxide),
+                        max = max(train_data$nitrogen.oxide)
+                           ),
+            numericInput(inputId = "room", 
+                         label = "Room",
+                         value = 0),
+            numericInput(inputId = "age", 
+                         label = "Age",
+                         value = 0),
+            numericInput(inputId = "dist.to.work", 
+                         label = "Distance to Employment Center",
+                         value = 0),
+            numericInput(inputId = "highway.index", 
+                         label = "Radial highways Accessibility",
+                         value = 0),
+            numericInput(inputId = "property.tax", 
+                         label = "Property Tax",
+                         value = 0),
+            numericInput(inputId = "pt.ratio", 
+                         label = "Pupil-teacher ratio",
+                         value = 0),
+            numericInput(inputId = "black", 
+                         label = "Black Community Proportion",
+                         value = 0),
+            numericInput(inputId = "lstat", 
+                         label = "Lower Status Proportion",
+                         value = 0),
+            actionButton("SubmitButton", "Submit", class = "btn btn-primary")),
+           
+           # main panel
+           mainPanel(
+             tags$label(h3("Status")),
+             verbatimTextOutput("contents"),
+             tableOutput("tabledata"))
+           ), # tab panel Home 
   
-  mainPanel(
-    tags$label(h3("Status")),
-    verbatimTextOutput("contents"),
-    tableOutput("tabledata"),
-    tags$label(h3("Kar")))
-  
-  ) # Close tab panel Home 
-  ) 
+  tabPanel("About",
+           div(includeMarkdown("about.md"),
+               align = "justify")
+           ) # tab panel About
+  ) # navbarPage 
+)# fluidPage 
 
 
 #--------------------------------------------------------------------------
@@ -134,16 +139,31 @@ server <- function(input, output, session) {
     df <- rbind(df, house.value)
     
     # pivot wider by transpose
-    df2 <- transpose(df)
+    df2 <- df %>% pivot_wider(names_from = Name, values_from = value)
     
     # Save it into local file
-    write.table(df2, "Shiny_InputValues.csv", row.names = F, col.names = F, quote = F, sep = ",")
+    write.csv(df2, "Shiny_InputValues.csv", row.names = F)
     
     # Read it back into R 
     new.test.set <- read.csv("Shiny_InputValues.csv", header = T)
     
-    new.test.set <- new.test.set %>% mutate(charles.river = as.factor(charles.river))
-    
+    new.test.set <- new.test.set %>% 
+      mutate(charles.river = as.factor(charles.river),
+             crime.rate = as.double(crime.rate),
+             resid.zone = as.double(resid.zone),
+             indus.biz = as.double(indus.biz),
+             nitrogen.oxide = as.double(nitrogen.oxide),
+             room = as.double(room),
+             age = as.double(age),
+             dist.to.work = as.double(dist.to.work),
+             highway.index = as.double(highway.index),
+             property.tax = as.double(property.tax),
+             pt.ratio = as.double(pt.ratio),
+             black = as.double(black),
+             lstat = as.double(lstat),
+             house.value = as.double(house.value)
+             ) 
+ 
     # Prediction using the Random Forest Model
     Output <- data.frame(Prediction = predict(model_rf, new.test.set))
     print(Output)
